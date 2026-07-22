7f72ce4034aabdaa3d22fe1c73cce199f9fa21e1
DHL <david.hernandez@uclm.es>
2026-07-22 19:04:43 +0200

    Commit and push: "Develop refactor, first step"

--- src/pyLibProcesses/__init__.py
+++ src/pyLibProcesses/__init__.py
@@ -1,7 +1,7 @@
-from defs import defs_processes
-from defs import defs_project
-from gui import ProjectProcessesDialog
-from gui import ProcessesManagerDialog
+from .defs import defs_processes
+from .defs import defs_project
+from .gui import ProjectProcessesDialog
+from .gui import ProcessesManagerDialog
 
 __all__ = [
     "defs_processes",
